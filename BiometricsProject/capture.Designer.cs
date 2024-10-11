6e864e2f6dc8f3395c0b039750cb3315ebc09b16
lemos-ctrl <gabuterolemuel@gmail.com>
2024-10-11 23:46:06 +0800

    responsive design for bms

--- BiometricsProject/capture.Designer.cs
+++ BiometricsProject/capture.Designer.cs
@@ -36,13 +36,14 @@
             this.fname = new System.Windows.Forms.TextBox();
             this.start_scan = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.fImage)).BeginInit();
             this.SuspendLayout();
             // 
             // fImage
             // 
             this.fImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.fImage.Location = new System.Drawing.Point(12, 12);
+            this.fImage.Location = new System.Drawing.Point(15, 12);
             this.fImage.Name = "fImage";
             this.fImage.Size = new System.Drawing.Size(301, 321);
             this.fImage.TabIndex = 0;
@@ -74,7 +75,7 @@
             // 
             // fname
             // 
-            this.fname.Location = new System.Drawing.Point(330, 339);
+            this.fname.Location = new System.Drawing.Point(330, 359);
             this.fname.Name = "fname";
             this.fname.Size = new System.Drawing.Size(458, 20);
             this.fname.TabIndex = 4;
@@ -94,11 +95,21 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(327, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
             // capture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.start_scan);
             this.Controls.Add(this.fname);
             this.Controls.Add(this.StatusLabel);
@@ -124,5 +135,6 @@
         private System.Windows.Forms.TextBox fname;
         private System.Windows.Forms.Button start_scan;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- BiometricsProject/main.Designer.cs
+++ BiometricsProject/main.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class main
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,55 +13,110 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
             this.enrol_btn = new System.Windows.Forms.Button();
             this.verify_btn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
+            this.flowLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel.ColumnCount = 1;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.flowLayoutPanel, 0, 1);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 3;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.564103F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95.72649F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1.709402F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(792, 468);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel.AutoSize = true;
+            this.flowLayoutPanel.Controls.Add(this.label1);
+            this.flowLayoutPanel.Controls.Add(this.enrol_btn);
+            this.flowLayoutPanel.Controls.Add(this.verify_btn);
+            this.flowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(143, 94);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(506, 283);
+            this.flowLayoutPanel.TabIndex = 1;
+            this.flowLayoutPanel.WrapContents = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(492, 113);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SWU SHS-AMS BIOMETRIC SYSTEM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // enrol_btn
             // 
-            this.enrol_btn.Location = new System.Drawing.Point(302, 48);
+            this.enrol_btn.BackColor = System.Drawing.Color.LightBlue;
+            this.enrol_btn.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.enrol_btn.Location = new System.Drawing.Point(3, 116);
             this.enrol_btn.Name = "enrol_btn";
-            this.enrol_btn.Size = new System.Drawing.Size(172, 79);
+            this.enrol_btn.Size = new System.Drawing.Size(500, 79);
             this.enrol_btn.TabIndex = 0;
             this.enrol_btn.Text = "Enroll";
-            this.enrol_btn.UseVisualStyleBackColor = true;
+            this.enrol_btn.UseVisualStyleBackColor = false;
             this.enrol_btn.Click += new System.EventHandler(this.enrol_btn_Click);
             // 
             // verify_btn
             // 
-            this.verify_btn.Location = new System.Drawing.Point(302, 238);
+            this.verify_btn.BackColor = System.Drawing.Color.LightGreen;
+            this.verify_btn.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.verify_btn.Location = new System.Drawing.Point(3, 201);
             this.verify_btn.Name = "verify_btn";
-            this.verify_btn.Size = new System.Drawing.Size(172, 79);
+            this.verify_btn.Size = new System.Drawing.Size(500, 79);
             this.verify_btn.TabIndex = 1;
             this.verify_btn.Text = "Verify";
-            this.verify_btn.UseVisualStyleBackColor = true;
+            this.verify_btn.UseVisualStyleBackColor = false;
             this.verify_btn.Click += new System.EventHandler(this.verify_btn_Click);
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.verify_btn);
-            this.Controls.Add(this.enrol_btn);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::BiometricsProject.Properties.Resources.swubg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(792, 468);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "main";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Biometrics System";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.flowLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
-        #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
         private System.Windows.Forms.Button enrol_btn;
         private System.Windows.Forms.Button verify_btn;
+        private System.Windows.Forms.Label label1;
     }
 }
-
